60cf919287265cabec0ec90217c051e7ea52d6b4
8700theis <57965180+8700theis@users.noreply.github.com>
2022-03-27 15:15:55 +0200

    test

--- js/contact.js
+++ js/contact.js
@@ -4,23 +4,18 @@ import FetchMyData from './components/FetchMyData.js';
 
 //Getting elements for templating
 const navElement = document.querySelector('.nav');
-const footerElement = document.querySelector('#footer');
 
 //Inserting templates
 navElement.insertAdjacentHTML('beforeend', navigationTemplate());
 
-//const footerMail = document.querySelector('.footer_middle_mail');
-//const footerMiddleText = document.querySelector('.footer_middle_text');
 const footerBottomText = document.querySelector('.footer_bottom_textCon_text');
+const formSubmitBtn = document.querySelector('.contact_bottom_formCon_button');
 
 //Fetching all texts
 const textsFetch = FetchMyData({ Endpoint: "texts" });
 textsFetch.then((texts) => {
     console.log(texts);
-    //footerMail.innerText = texts[2].description;
-    //footerMail.setAttribute('href', `mailto:${texts[2].description}`);
     let footerTextArray = texts[3].description.split(/(©)/);
-    //footerMiddleText.innerText = footerTextArray[0];
     footerBottomText.innerText = footerTextArray[1] + footerTextArray[2];
 });
 
@@ -29,3 +24,74 @@ const imagesFetch = FetchMyData({ Endpoint: "images" });
 imagesFetch.then((images) => {
     console.log(images);
 });
+
+const validateForm = () => {
+    let email = document.querySelector('#email');
+    let message = document.querySelector('#message');
+    let emailImg = document.querySelector('.emailImg');
+    let messageImg = document.querySelector('.messageImg');
+    let result = true;
+    let emailRegex =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!emailRegex.test(email.value)) {
+        if (email.classList.contains("valid")) {
+            email.classList.remove("valid");
+        }
+        email.classList.add("unvalid");
+        emailImg.src = '../images/icons/crossed.png';
+        result = false;
+    } else {
+        if (email.classList.contains("unvalid")) {
+            email.classList.remove("unvalid");
+        }
+        email.classList.add("valid");
+        emailImg.src = '../images/icons/check.png'
+    }
+
+    if (!message.value.length > 0) {
+        if (message.classList.contains("valid")) {
+            message.classList.remove("valid");
+        }
+        message.classList.add("unvalid");
+        messageImg.src = '../images/icons/crossed.png';
+        result = false;
+    } else {
+        if (message.classList.contains("unvalid")) {
+            message.classList.remove("unvalid");
+        }
+        message.classList.add("valid");
+        messageImg.src = '../images/icons/check.png'
+    }
+
+    if (result) {
+        const emailInputValue = email.value;
+        const messageInputValue = message.value;
+
+        var formdata = new FormData();
+        formdata.append("email", emailInputValue);
+        formdata.append("message", messageInputValue);
+
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+        fetch("https://modelin.webexam-mcdm.dk/api/contacts", requestOptions)
+            .then(response => response.text())
+            .then(theResult => {
+                alert(theResult)
+            })
+            .catch(error => console.log('error', error));
+    }
+}
+
+formSubmitBtn.addEventListener('click', () => validateForm());
+
+const resetFormOnLoad = () => {
+    document.querySelector("#email").value = '';
+    document.querySelector("#message").value = '';
+};
+
+window.onload = resetFormOnLoad;
